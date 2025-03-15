629b2ea4a89dde16dceae579322c9d2e98dac847
mi-haru013 <s.c.xx.lrv6@gmail.com>
2025-03-15 17:59:05 +0900

    Add files via upload

---
+++ kadai1/kadai1.js
@@ -0,0 +1,21 @@
+// 変数の宣言
+let weather;
+let text;
+
+// 値の代入
+weather = '今日は晴れです';
+text = 'こんにちは';
+
+// コンソールへの出力
+console.log(text + weather);
+
+
+// 変数の宣言・値の代入
+let x = 10;
+let y =3;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+
